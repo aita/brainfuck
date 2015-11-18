9949b13903f9376fd5d0926bcb30bfbc05ba3fa1
Ryo Aita <ryoait@gmail.com>
2015-11-19 00:58:01 +0900

    implement byte code interpreter

--- brainfuck.c
+++ brainfuck.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define TAPE_LENGTH 30000
 #define MAX_LOOP 1000
+#define PROGRAM_SIZE 100000
 
+enum {
+    NOP,
+    ADD,
+    SUB,
+    RIGHT,
+    LEFT,
+    LOOP,
+    ENDLOOP,
+    EXIT,
+    PUTC,
+    GETC,
+    NUM_OPCODES
+};
+
+typedef unsigned char bf_opcode_t;
 
 typedef struct bf_state {
     unsigned char tape[TAPE_LENGTH];
@@ -13,8 +30,9 @@ typedef struct bf_state {
 } bf_state_t;
 
 typedef struct bf_program {
-    unsigned char *tape;
-    size_t len;
+    // unsigned char *opcodes;
+    // size_t len;
+    bf_opcode_t opcodes[PROGRAM_SIZE];
 } bf_program_t;
 
 static void run(bf_state_t *, bf_program_t *);
@@ -36,58 +54,64 @@ bf_run(bf_program_t *program) {
 
 static void
 run(bf_state_t *s, bf_program_t *p) {
-    unsigned char *c = p->tape;
-    unsigned char *end = p->tape + p->len;
     int nskip = 0;
     int nloop = 0;
-    unsigned char *loops[MAX_LOOP] = {0};
+    bf_opcode_t *loops[MAX_LOOP] = {0};
+    bf_opcode_t *op = &p->opcodes[0];
 
-    for (; c != p->tape + p->len; c++) {
-        // printf("%d, %d, %c\n", nskip, nloop, *c);
-        switch (*c) {
-            case '>':
+    for (;;) {
+        int n;
+
+        switch (*op++) {
+            case NOP:
+                break;
+            case RIGHT:
+                n = *op++;
                 if (!nskip) {
-                    s->ptr++;
+                    s->ptr += n;
                 }
                 break;
-            case '<':
+            case LEFT:
+                n = *op++;
                 if (!nskip) {
-                    s->ptr--;
+                    s->ptr -= n;
                 }
                 break;
-            case '+':
+            case ADD:
+                n = *op++;
                 if (!nskip) {
-                    (*s->ptr)++;
+                    (*s->ptr) += n;
                 }
                 break;
-            case '-':
+            case SUB:
+                n = *op++;
                 if (!nskip) {
-                    (*s->ptr)--;
+                    (*s->ptr) -= n;
                 }
                 break;
-            case '.':
+            case PUTC:
                 if (!nskip) {
                     s->put_char(s, *s->ptr);
                 }
                 break;
-            case ',':
+            case GETC:
                 if (!nskip) {
                     *s->ptr = (unsigned char)s->get_char(s);
                 }
                 break;
-            case '[':
-                loops[nloop++] = c;
+            case LOOP:
+                loops[nloop++] = op;
                 if (!*s->ptr) {
                     nskip++;
                 }
                 break;
-            case ']':
+            case ENDLOOP:
                 if (nloop < 1) {
-                    fprintf(stderr, "] without maching [\n");
-                    break;
+                    fprintf(stderr, "bad loop\n");
+                    return;
                 }
                 if (*s->ptr) {
-                    c = loops[nloop-1];
+                    op = loops[nloop-1];
                 } else {
                      nloop--;
                 }
@@ -95,10 +119,78 @@ run(bf_state_t *s, bf_program_t *p) {
                     nskip--;
                 }
                 break;
+            case EXIT:
+                return;
+            default:
+                fprintf(stderr, "invalid opcode: %d\n", *op);
+                return;
+        }
+    }
+}
+
+static bf_program_t *
+bf_compile(const char *c) {
+    bf_program_t *p;
+    bf_opcode_t *op;
+
+    if ((p = malloc(sizeof(bf_program_t))) == NULL) {
+        return NULL;
+    }
+    memset(p->opcodes, NOP, sizeof(p->opcodes));
+
+    op = &p->opcodes[0];
+    for (;;) {
+        int n;
+
+        switch (*c++) {
+            case '>':
+                *op++ = RIGHT;
+                for(n = 1; *c == '<' && n < 256; n++, c++);
+                *op++ = n;
+                break;
+            case '<':
+                *op++ = LEFT;
+                for(n = 1; *c == '>' && n < 256; n++, c++);
+                *op++ = n;
+                break;
+            case '+':
+                *op++ = ADD;
+                for(n = 1; *c == '+' && n < 256; n++, c++);
+                *op++ = n;
+                break;
+            case '-':
+                *op++ = SUB;
+                for(n = 1; *c == '-' && n < 256; n++, c++);
+                *op++ = n;
+                break;
+            case '.':
+                *op++ = PUTC;
+                break;
+            case ',':
+                *op++ = GETC;
+                break;
+            case '[':
+                *op++ = LOOP;
+                break;
+            case ']':
+                *op++ = ENDLOOP;
+                break;
             case 0:
+                *op = EXIT;
+                goto exit;
+            default:
                 break;
         }
     }
+
+exit:
+    return p;
+}
+
+void
+bf_program_delete(bf_program_t *p) {
+    // free(p->opcodes);
+    // free(p);
 }
 
 static void
@@ -116,7 +208,8 @@ int
 main(int argc, char **argv) {
     FILE *fp;
     size_t len;
-    bf_program_t program;
+    bf_program_t *program;
+    char *codes;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <filename>\n", *argv);
@@ -131,7 +224,7 @@ main(int argc, char **argv) {
         perror("fseek");
         return 1;
     }
-    if ((program.len = ftell(fp)) < 0) {
+    if ((len = ftell(fp)) < 0) {
         perror("ftell");
         return 1;
     }
@@ -139,11 +232,12 @@ main(int argc, char **argv) {
         perror("fseek");
         return 1;
     }
-    if ((program.tape = malloc(program.len)) == NULL) {
+    if ((codes = malloc(len+1)) == NULL) {
          perror("malloc");
          return 1;
     }
-    if (fread(program.tape, 1, program.len, fp) < 0) {
+    codes[len] = 0;
+    if (fread(codes, 1, len, fp) < 0) {
          perror("fread");
          return 1;
     }
@@ -152,7 +246,10 @@ main(int argc, char **argv) {
         return 1;
     }
 
-    bf_run(&program);
-    free(program.tape);
+    program = bf_compile(codes);
+    free(codes);
+    bf_run(program);
+    bf_program_delete(program);
+
     return 0;
 }
